3a25d716ce931273252ea0f9bc46c1eda6f1ec31
Your Name <you@example.com>
2023-06-07 23:14:49 +1000

    addded colour to toggle switch output

--- public/script.js
+++ public/script.js
@@ -2,8 +2,7 @@
 const form = document.getElementById("songform");
 const songlist = document.getElementById("songlist");
 const toggleSwitch = document.getElementById("toggleSwitch");
-const output = document.getElementById("output");
-const image = document.getElementById("image");
+
 
 var songList = [];
 
@@ -28,25 +27,15 @@ function addSong(title, vibe, album, artist) {
       form.elements.songVibe.value,
       form.elements.songAlbum.value,
       form.elements.songArtist.value,
-      form.elements.toggleSwitch.value,
     )
   })
-
-  toggleSwitch.addEventListener('change', function() {
-    if (toggleSwitch.checked) {
-      output.innerHTML = '<img src="chill.png">';
-    } else {
-      output.innerHTML = '<img src="sad.png">';
-    }
-
-  });
 
   function displaySong(song) {
     let item = document.createElement("li");
     item.setAttribute("data-id", song.id);
     item.innerHTML = 
       `<p>${song.vibe}</p><p>${song.title}</p><p>${song.artist}</p><p>${song.album}</p>
-      <span>${output}</span>
+      <span></span>
       `;
   
     songlist.appendChild(item);
@@ -80,15 +69,35 @@ function addSong(title, vibe, album, artist) {
     let isChecked = event.target.checked;
     if (song.id == item.getAttribute("data-id")) {
       if (isChecked) {
-        item.style.backgroundColor = "rgb(220, 255, 220)";
+        item.style.backgroundColor = "#2E2D2D";
         song.billable = true;
         console.log(songList);
       } else {
-        item.style.backgroundColor = "white";
+        item.style.backgroundColor = "#725A58";
         song.billable = false;
         console.log(songList);
       }
     }
   }); */
 
-  }
+  toggleSwitch.addEventListener("change", function(event) {
+    let isChecked = event.target.checked;
+    if (song.id == item.getAttribute("data-id")) {
+      if (isChecked) {
+        item.style.backgroundColor = "#2E2D2D";
+        song.billable = true;
+        console.log(songList);
+      } else {
+        item.style.backgroundColor = "#725A58";
+        song.billable = false;
+        console.log(songList);
+      }
+      if (isChecked) {
+        return localStorage.setItem();
+      }
+    }
+  }); 
+
+  
+
+}
